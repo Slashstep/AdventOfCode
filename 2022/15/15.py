2232742725d49b6c1ead0c3407c44ef3e26918cf
Slashstep <slashstep@outlook.de>
2022-12-16 15:23:55 +0100

    Day 15 Part 2 Finally

--- 2022/15/15.py
+++ 2022/15/15.py
@@ -1,12 +1,13 @@
-# Part 1: x < 4424278 Part 2: ####
-# distress beacon pos 0 < xy < 4.000.000
-# tuning freq x*4000000+y
+# Part 1: x < 4424278 Part 2: 10382630753392
+
+from math import inf
+
+limitRight = 4000000
 
 lines = []
 sensoryCor = []
 beaconCor = []
 sbDistances = []
-lineCoverage = []
 
 def inputToArray():
     global lines
@@ -20,47 +21,57 @@ def parseInputToCoordinates(arr):
         sensoryCor.append((int(split[2][2:-1:]), int(split[3][2:-1:])))
         beaconCor.append((int(split[8][2:-1:]), int(split[9][2:-1:])))
 
+def dist(x1, x2, y1, y2):
+    return abs(x1-x2) + abs(y1 - y2)
+
 def calculateSensorBeaconDistance(sensors, beacons):
     for i, sensor in enumerate(sensors):
-        dist = abs(sensor[0]-beacons[i][0]) + abs(sensor[1] - beacons[i][1])
-        sbDistances.append(dist)
+        sbDistances.append(dist(sensor[0], beacons[i][0], sensor[1], beacons[i][1]))
 
 def calculateLineCovarage(line, sensors, distances):
+    minX, maxX = inf, -inf
     for i in range(0, len(distances), 1):
         yDist = abs(sensors[i][1]-line)
         if distances[i] >= yDist:
             restDist = abs(distances[i] - yDist)
-            for l in range(-int(restDist), int(restDist)+1, 1):
-                lineCoverage.append(sensors[i][0]+l)
+            xLeft = sensors[i][0]-restDist
+            xRight = sensors[i][0]+restDist
+
+            minX = min(xLeft, minX)
+            maxX = max(xRight, maxX)
+
+    return maxX-minX
+
+def checkEachLine(i):
+    coveredRanges = []
+    for j in range(0, len(sbDistances), 1):
+        yDist = abs(sensoryCor[j][1]-i)
+        if yDist > sbDistances[j]:
+            continue
+        restDist = sbDistances[j] - yDist
+        xLeft = sensoryCor[j][0]-restDist
+        xRight = sensoryCor[j][0]+restDist
+        
+        coveredRanges.append((xLeft, xRight))
+    coveredRanges.sort()
+
+    curMaxRight = coveredRanges[0][1]
+    for x in range(0, len(coveredRanges), 1):
+        if coveredRanges[x][0] > curMaxRight:
+            return curMaxRight + 1
+        curMaxRight = max(curMaxRight, coveredRanges[x][1])
     
-    return len(set(lineCoverage))-1
+    return False
 
-def calculateFreePosOnLine(line, sensors, distances):
-    for i in range(0, len(distances), 1):
-        yDist = abs(sensors[i][1]-line)
-        if distances[i] >= yDist:
-            restDist = abs(distances[i] - yDist)
-            if sensors[i][0] - restDist < 0:
-                leftX = 0
-            else:
-                leftX = -restDist
-            if sensors[i][0] + restDist > 4000000:
-                rightX = 4000000
-            else:
-                rightX = restDist
-            for l in range(leftX , rightX, 1):
-                lineCoverage.append(sensors[i][0]+l)
-    
-    return len(set(lineCoverage))-1
-
-def findDistressPosition():
-    for i in range(0, 4000000, 1):
-        print(calculateFreePosOnLine(i, sensoryCor, sbDistances))
-            
-
+def checkPart2():
+    for i in range(0, limitRight, 1):
+        x = checkEachLine(i)
+        if x != False:
+            print(x * 4000000 + i)
+            break
 
 inputToArray()
 parseInputToCoordinates(lines)
 calculateSensorBeaconDistance(sensoryCor, beaconCor)
-#print(calculateLineCovarage(2000000, sensoryCor, sbDistances))
-findDistressPosition()
+print(calculateLineCovarage(2000000, sensoryCor, sbDistances))
+checkPart2()
