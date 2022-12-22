d6b45d23282c667b8828a53ec802221c16c3b3e6
Slashstep <slashstep@outlook.de>
2022-12-22 23:42:08 +0100

    Day 18 Pt 2

--- 2022/18/18.py
+++ 2022/18/18.py
@@ -1,4 +1,4 @@
-# Part 1: 3530, Part 2: ####
+# Part 1: 3530, Part 2: 2000
 
 lines = []
 coordinates = []
@@ -17,26 +17,58 @@ def CreateDroplet():
         
         droplet[int(x)+1][int(y)+1][int(z)+1] = 1
 
-def FindTotalSurface():
+def FindTotalSurface(num2Check):
     sCounter = 0
     for c in coordinates:
         #check x
-        sCounter += 1 if c[0] + 1 < 25 and droplet[c[0]+1][c[1]][c[2]] == 0 else 0
-        sCounter += 1 if c[0] - 1 >= 0 and droplet[c[0]-1][c[1]][c[2]] == 0 else 0
+        sCounter += 1 if c[0] + 1 < 25 and droplet[c[0]+1][c[1]][c[2]] == num2Check else 0
+        sCounter += 1 if c[0] - 1 >= 0 and droplet[c[0]-1][c[1]][c[2]] == num2Check else 0
 
         #check y
-        sCounter += 1 if c[1] + 1 < 25 and droplet[c[0]][c[1]+1][c[2]] == 0 else 0
-        sCounter += 1 if c[1] - 1 >= 0 and droplet[c[0]][c[1]-1][c[2]] == 0 else 0
+        sCounter += 1 if c[1] + 1 < 25 and droplet[c[0]][c[1]+1][c[2]] == num2Check else 0
+        sCounter += 1 if c[1] - 1 >= 0 and droplet[c[0]][c[1]-1][c[2]] == num2Check else 0
 
         #check z
-        sCounter += 1 if c[2] + 1 < 25 and droplet[c[0]][c[1]][c[2]+1] == 0 else 0
-        sCounter += 1 if c[2] - 1 >= 0 and droplet[c[0]][c[1]][c[2]-1] == 0 else 0
+        sCounter += 1 if c[2] + 1 < 25 and droplet[c[0]][c[1]][c[2]+1] == num2Check else 0
+        sCounter += 1 if c[2] - 1 >= 0 and droplet[c[0]][c[1]][c[2]-1] == num2Check else 0
 
     print(sCounter)
 
 def FindOutsideSurface():
-    print()
+    isTotalMovementPossible = True
+    while isTotalMovementPossible == True:
+        isWaterPossible = True
+        x = 0
+        y = 0
+        z = 0
+        while isWaterPossible == True:
+            if x+1 < 25 and droplet[x+1][y][z] == 0:
+                x += 1
+            elif y+1 < 25 and droplet[x][y+1][z] == 0:
+                y += 1
+            elif z+1 < 25 and droplet[x][y][z+1] == 0:
+                z += 1
+            else:
+                droplet[x][y][z] = 2
+                isWaterPossible = False
+                if x == 0 and y == 0 and z == 0:
+                    isTotalMovementPossible = False
+
+    hasOutsideZeros = True
+    while hasOutsideZeros == True:
+        hasOutsideZeros = False
+        for i in range(len(droplet)):
+            for j in range(len(droplet[i])):
+                for k in range(len(droplet[i][j])):
+                    if droplet[i][j][k] == 0:
+                        #Check neighbors for 2
+                            if droplet[i+1][j][k] == 2 or droplet[i-1][j][k] == 2 or droplet[i][j+1][k] == 2 or droplet[i][j-1][k] == 2 or droplet[i][j][k+1] == 2 or droplet[i][j][k-1] == 2:
+                                droplet[i][j][k] = 2
+                                hasOutsideZeros = True
+
+    FindTotalSurface(2)
 
 InputToArray()
 CreateDroplet()
-FindTotalSurface()
+FindTotalSurface(0)
+FindOutsideSurface()
