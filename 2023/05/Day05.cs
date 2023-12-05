c55c726b916ce92ad4e82accd214462ceb0eda4a
Slashstep <slashstep@outlook.de>
2023-12-05 18:42:20 +0100

    Day05

--- 2023/05/Day05.cs
+++ 2023/05/Day05.cs
@@ -24,72 +24,125 @@ class Day05{
         return lines;
     }
 
-    static ScratchCard ReadScratCard(string input){
-        ScratchCard scratchCard= new ScratchCard();
+    static List<long> SeparateSeeds(){
+        List<long> seeds = new List<long>();
 
-        string[] cutID = input.Split(": ");
-        string[] cutWinDraw = cutID[1].Split(" | ");
-        string[] wins = cutWinDraw[0].Split(" ");
-        string[] draws = cutWinDraw[1].Split(" ");
+        string[] leftRight = Input[0].Split(": ");
+        string[] rightNum = leftRight[1].Split(" ");
 
-        //Put in Wins
-        for (int i = 0; i < wins.Length; i++){
-            if (wins[i] == "")
-                continue;
-
-            scratchCard.WinningNumbers.Add(int.Parse(wins[i]));
+        foreach (string s in rightNum){
+            seeds.Add(long.Parse(s));
         }
 
-        for (int i = 0; i < draws.Length; i++){
-            if (draws[i] == "")
-                continue;
+        return seeds;
+    }
 
-            scratchCard.DrawnNumbers.Add(int.Parse(draws[i]));
+    static List<List<Map>> GenerateMaps(){
+        List<List<Map>> maps = new List<List<Map>>();
+        List<Map> curMap = new List<Map>();
+        for (int i = 3; i < Input.Count(); i++){
+            if (Input[i] == ""){
+                curMap = curMap.OrderBy(m => m.Source).ToList();
+                maps.Add(curMap);
+                curMap = new List<Map>();
+                i++;
+            }
+            else{
+                string[] line = Input[i].Split(" ");
+                long source = long.Parse(line[1]);
+                long dest = long.Parse(line[0]);
+                long length = long.Parse(line[2]);
+                curMap.Add(new Map(source, dest - source, length));
+            }
         }
+        curMap = curMap.OrderBy(m => m.Source).ToList();
+        maps.Add(curMap);
 
-        scratchCard.CheckWinning();
-
-        return scratchCard;
+        return maps;
     }
 
     static void Part1(){
-        List<ScratchCard> ScratchCards = new List<ScratchCard>();
+        List<long> seeds = SeparateSeeds();
+        List<List<Map>> maps = GenerateMaps();
 
-        for (int i = 0; i < Input.Count(); i++)
-            ScratchCards.Add(ReadScratCard(Input[i]));
-
-        int counter = 0;
-        for (int i = 0; i < ScratchCards.Count(); i++){
-            counter += ScratchCards[i].Value;
-        }
-
-        Console.WriteLine(counter);
-    }
-
-    static void Part2(){
-        List<ScratchCard> ScratchCards = new List<ScratchCard>();
-
-        for (int i = 0; i < Input.Count(); i++)
-            ScratchCards.Add(ReadScratCard(Input[i]));
-
-        int counter = 0;
-        for (int i = 0; i < ScratchCards.Count(); i++){
-            for (int j = 0; j < ScratchCards[i].Copies; j++){
-                int val = ScratchCards[i].Wins;
-                
-                for (int k = i + 1; k <= MathF.Min(val + i, ScratchCards.Count() - 1); k++){
-                    ScratchCards[k].Copies++;
+        long maxLowest = 10000000000000;
+        for (int h = 0; h < seeds.Count(); h++){
+            long s = seeds[h];
+            for (int i = 0; i < maps.Count(); i++){
+                for (int j = 0; j < maps[i].Count(); j++){
+                    long diff = maps[i][j].CheckMap(s);
+                    if (diff != 0){
+                        s = diff;
+                        break;
+                    }
                 }
             }
 
-            counter += ScratchCards[i].Copies;
+            if (s < maxLowest){
+                maxLowest = s;
+            }
         }
 
-        Console.WriteLine(counter);
+        Console.WriteLine(maxLowest);
     }
 
-    //Part 1: 15205
-    //Part 2: 6189740
+    static void Part2(){
+        List<long> seeds = SeparateSeeds();
+        List<List<Map>> maps = GenerateMaps();
+
+        long maxLowest = 10000000000000;
+        for (int g = 0; g < seeds.Count(); g += 2){
+            List<Range> ranges = new List<Range>();
+            ranges.Add(new Range(seeds[g], seeds[g] + seeds[g + 1]));
+
+            foreach (List<Map> l in maps){
+                ranges = CheckMapRange(ranges, l);
+            }
+
+            ranges = ranges.OrderBy(l => l.Start).ToList();
+
+            if (ranges[0].Start < maxLowest){
+                maxLowest = ranges[0].Start;
+            }
+        }
+        Console.WriteLine(maxLowest);
+    }
+
+    static List<Range> CheckMapRange(List<Range> ranges, List<Map> maps){
+        List<Range> newRanges = new List<Range>();
+
+        foreach (Range r in ranges){
+            foreach (Map m in maps){
+                //Check if Range is completly in Map
+                if (r.Start >= m.Source && r.Start < (m.Source + m.Length) && r.End > m.Source && r.End < (m.Source + m.Length)){
+                    newRanges.Add(new Range(r.Start + m.Diff, r.End + m.Diff));
+                    r.End = r.Start;
+                    break;
+                }
+
+                //Range starts left from Map
+                if (r.Start < m.Source && r.End > m.Source && r.End < m.Source + m.Length){
+                    newRanges.Add(new Range(m.Source + m.Diff, r.End + m.Diff));
+                    r.End = m.Source;
+                }
+
+                //Range end right from Map
+                if (r.Start >= m.Source && r.Start < m.Source + m.Length && r.End >= m.Source + m.Length){
+                    newRanges.Add(new Range(r.Start + m.Diff, m.Source + m.Length - 1 + m.Diff));
+                    r.Start = m.Source + m.Length;
+                }
+            }
+            if (r.Start - r.End < 0){
+                newRanges.Add(r);
+            }
+        }
+
+        return newRanges;
+    }
+
+    //Part 1: 525792406
+    //Part 2: 79004094 goal
+
     public static void Main(string[] args){
         Input = ReadFile();
         Part1();
@@ -97,38 +150,41 @@ class Day05{
     }
 }
 
-class ScratchCard{
-    public List<int> WinningNumbers = new List<int>();
-    public List<int> DrawnNumbers = new List<int>();
-    public int Wins;
-    public int Value;
+class Range{
+    public long Start;
+    public long End;
 
-    public int Copies;
-
-    public ScratchCard(){
-        WinningNumbers = new List<int>();
-        DrawnNumbers = new List<int>();
-        Wins = 0;
-        Value = 0;
-        Copies = 1;
+    public Range(long start, long end){
+        this.Start = start;
+        this.End = end;
     }
 
-    public void CheckWinning(){
-        int counter = 0;
-        
-        for (int i = 0; i < WinningNumbers.Count(); i++){
-            if (DrawnNumbers.Contains(WinningNumbers[i])){
-                counter++;
-            }
-        }
+    public void PrintRange(){
+        Console.WriteLine(Start.ToString() + " -> " + End.ToString());
+    }
+}
 
-        if (counter == 0){
-            Value = 0;
+class Map{
+    public long Source;
+    public long Diff;
+    public long Length;
+
+    public Map(long source, long diff, long length){
+        Source = source;
+        Diff = diff;
+        Length = length;
+    }
+
+    public void PrintMap(){
+        Console.WriteLine(Source.ToString() + ", " + Length.ToString() + ", " + Diff.ToString());
+    }
+
+    public long CheckMap(long seed){
+        if (Source <= seed && seed < Source + Length){
+            return seed + Diff;
         }
         else{
-            Value = (int)Math.Pow(2, counter - 1);
+            return 0;
         }
-
-        Wins = counter;
     }
 }
