6ab36499fcc3711b3f313f12b7945b8aa6630631
Slashstep <slashstep@outlook.de>
2023-12-08 15:53:59 +0100

    Day08

--- 2023/08/Day08.cs
+++ 2023/08/Day08.cs
@@ -25,18 +25,121 @@ class Day08{
     }
 
     static void Part1(){
+        string lr = Input[0];
+        List<Instruction> instructions = new List<Instruction>();
 
+        for (int i = 2; i < Input.Count; i++){
+            string[] nodeLR = Input[i].Split(" = ");
+            string[] lrNode = nodeLR[1].Split(", ");
+            Instruction newInstruction = new Instruction(nodeLR[0], lrNode[0].Substring(1), lrNode[1].Substring(0, 3));
+            instructions.Add(newInstruction);
+        }
+
+        instructions = instructions.OrderBy(i => i.Node).ToList();
+        
+        Console.WriteLine(FindInstruction(instructions, lr, instructions[0], 0));
+    }
+
+    static long FindInstruction(List<Instruction> inst, string lr, Instruction next, long counter){
+        if (next.Node[2].ToString() == "Z"){
+            return counter;
+        }
+
+        if (next.Left == next.Node && next.Right == next.Node){
+            Console.WriteLine("Emergeny Stop");
+            return counter;
+        }
+
+
+        int nextString = (int)(counter % (long)lr.Length);
+        if (lr[nextString].ToString() == "L"){
+            Instruction newInstruction = inst.Find(n => n.Node == next.Left);
+            counter = FindInstruction(inst, lr, newInstruction, ++counter);
+        }
+        else{
+            Instruction newInstruction = inst.Find(n => n.Node == next.Right);
+            counter = FindInstruction(inst, lr, newInstruction, ++counter);
+        }
+
+        return counter;
     }
 
     static void Part2(){
+        string lr = Input[0];
+        List<Instruction> instructions = new List<Instruction>();
 
+        for (int i = 2; i < Input.Count; i++){
+            string[] nodeLR = Input[i].Split(" = ");
+            string[] lrNode = nodeLR[1].Split(", ");
+            Instruction newInstruction = new Instruction(nodeLR[0], lrNode[0].Substring(1), lrNode[1].Substring(0, 3));
+            instructions.Add(newInstruction);
+        }
+
+        foreach(Instruction i in instructions){
+            i.LeftI = instructions.IndexOf(instructions.Find(n => n.Node == i.Left));
+            i.RightI = instructions.IndexOf(instructions.Find(n => n.Node == i.Right));
+        }
+
+        instructions = instructions.OrderBy(i => i.Node).ToList();
+
+        List<long> longvals = new List<long>();
+        foreach (Instruction inst in instructions){
+            if (inst.Node[2].ToString() == "A"){
+                long c = FindInstruction(instructions, lr, inst, 0);
+                longvals.Add(c);
+            }
+        }
+
+        while (longvals.Count > 1){
+            longvals[0] = lcm(longvals[0], longvals[1]);
+            longvals.RemoveAt(1);
+        }
+
+        Console.WriteLine(longvals[0]);
     }
 
-    //Part 1: 
-    //Part 2: 
+    static long gcf(long a, long b)
+    {
+        while (b != 0)
+        {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static long lcm(long a, long b)
+    {
+        return (a / gcf(a, b)) * b;
+    }
+
+    //Part 1: 20569
+    //Part 2: 21366921060721
     public static void Main(string[] args){
         Input = ReadFile();
         Part1();
         Part2();
+    }
+}
+
+class Instruction{
+    public string Node;
+    public string Left;
+    public string Right;
+    public int LeftI;
+    public int RightI;
+
+
+    public Instruction(string node, string left, string right){
+        this.Node = node;
+        this.Left = left;
+        this.Right = right;
+        LeftI = 0;
+        RightI = 0;
+    }
+
+    public void PrintInstruction(){
+        Console.WriteLine(Node + ": " + Left + ", " + Right);
     }
 }
