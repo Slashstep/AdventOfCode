04f824580bfb29ee2223051ef382247d9c7fa59c
Slashstep <slashstep@outlook.de>
2023-12-10 15:07:06 +0100

    Day10

--- 2021/11/Day11.cs
+++ 2021/11/Day11.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 
-class Day10{
+class Day11{
 
     static public List<string> Input = new List<string>();
 
@@ -24,38 +25,70 @@ class Day10{
         return lines;
     }
 
+    static List<Octopus> Octos = new List<Octopus>();
     static void Part1(){
-        int[,] octos = new int[10,10];
 
         for (int y = 0; y < 10; y++){
             for (int x = 0; x < 10; x++){
-                octos[y, x] = int.Parse(Input[y][x].ToString());
+                Octos.Add(new Octopus(int.Parse(Input[y][x].ToString()), new Vector2(x, y)));
             }
         }
 
-        long flashCounter = 0;
-        for (int i = 0; i < 100; i++){
-            bool hasSomethingChanged = true;
-            while (hasSomethingChanged){
-                for (int y = 0; y < 10; y++){
-                    for (int x = 0; x < 10; x++){
-                        octos[y, x]++;
+        PrintList(Octos);
 
-                        if (octos[y, x] > 9){
-                            for (int i = -1; i <= 1; i++){
-                                for (int j = -1; j <= 1; j++){
-                                    
-                                }
-                            }
-                        }
-                    }
-                }
+        for (int i = 0; i < 10; i++){
+            foreach(Octopus o in Octos){
+                o.Value++;
             }
+            foreach(Octopus o in Octos){
+                IncreaseOctopus(o);
+            }
+
+            PrintList(Octos);
         }
     }
 
     static void Part2(){
         
+    }
+
+    static void IncreaseOctopus(Octopus octo){
+        if (octo.isFlashing){
+            return;
+        }
+
+        if (octo.Value > 9){
+            octo.isFlashing = true;
+            octo.Value = 0;
+
+            for (int y = -1; y <= 1; y++){
+                for (int x = -1; x <= 1; x++){
+                    Octopus nextOctopus = Octos.Find(o => o.Pos == new Vector2(o.Pos.X + x, o.Pos.Y + y));
+
+                    if (nextOctopus == null){
+                        continue;
+                    }
+
+                    IncreaseOctopus(nextOctopus);
+                }
+            }
+        }
+
+        return;
+    }
+
+    static void PrintList(List<Octopus> o){
+        
+        string s = "";
+        for (int i = 0; i < o.Count; i++){
+            if (i % 10 == 0){
+                Console.WriteLine(s);
+                s = "";
+            }
+            s += o[i].Value.ToString();
+            
+        }
+        Console.WriteLine(s);
     }
 
     //Part 1: 
@@ -65,5 +98,17 @@ class Day10{
         Input = ReadFile();
         Part1();
         Part2();
+    }
+}
+
+class Octopus{
+    public int Value;
+    public bool isFlashing;
+    public Vector2 Pos;
+
+    public Octopus(int value, Vector2 pos){
+        this.Value = value;
+        isFlashing = false;
+        this.Pos = pos;
     }
 }
---
+++ 2023/10/Day10.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+class Day10{
+
+    static public List<string> Input = new List<string>();
+    static Vector2Int start = new Vector2Int(0,0);
+    static List<Vector2Int> loop = new List<Vector2Int>();
+
+    static List<string> ReadFile(){
+        List<string> lines = new List<string>();
+
+        try{
+            if (File.Exists("input.txt"))
+                lines.AddRange(File.ReadAllLines("input.txt"));
+            else
+                Console.WriteLine("The file does not exist.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+
+        return lines;
+    }
+
+    static Vector2Int FindStart(){
+        for (int i = 0; i < Input.Count; i++){
+            for (int j = 0; j < Input[i].Length; j++){
+                if (Input[i][j].ToString() == "S"){
+                    return new Vector2Int(j, i);
+                }
+            }
+        }
+        
+        return new Vector2Int(0, 0);
+    }
+
+    static void FindLoop(){
+        start = FindStart();
+        HashSet<int> val = new HashSet<int>();
+        List<Vector2Int> vecsToCheck = new List<Vector2Int>();
+        vecsToCheck.Add(start.left);
+        vecsToCheck.Add(start.right);
+        vecsToCheck.Add(start.up);
+        vecsToCheck.Add(start.down);
+
+        foreach (Vector2Int v in vecsToCheck){
+            loop = new List<Vector2Int>();
+            loop.Add(start);
+            loop = GetLoopPositions(v, 1, loop);
+            if (loop.Count > 0 && !val.Add(loop.Count)){
+                break;
+            }
+        }
+    }
+
+    static Vector2Int VectorToCheck(Vector2Int from, List<Vector2Int> visited, int counter){
+        switch (Input[from.Y][from.X].ToString()){
+            case "-":
+                if (CheckStart(from.left, counter)) return from.left;
+                else if (CheckStart(from.right, counter)) return from.right;
+                if (IsVectorInList(from.left, visited)) return from.right;
+                else return from.left;
+            case "|":
+                if (CheckStart(from.up, counter)) return from.up;
+                else if (CheckStart(from.down, counter)) return from.down;
+                if (IsVectorInList(from.up, visited)) return from.down;
+                else return from.up;
+            case "L":
+                if (CheckStart(from.up, counter)) return from.up;
+                else if (CheckStart(from.right, counter)) return from.right;
+                if (IsVectorInList(from.up, visited)) return from.right;
+                else return from.up;
+            case "F":
+                if (CheckStart(from.down, counter)) return from.down;
+                else if (CheckStart(from.right, counter)) return from.right;
+                if (IsVectorInList(from.right, visited)) return from.down;
+                else return from.right;
+            case "7":
+                if (CheckStart(from.left, counter)) return from.left;
+                else if (CheckStart(from.down, counter)) return from.down;
+                if (IsVectorInList(from.down, visited)) return from.left;
+                else return from.down;
+            case "J":
+                if (CheckStart(from.left, counter)) return from.left;
+                else if (CheckStart(from.up, counter)) return from.up;
+                if (IsVectorInList(from.left, visited)) return from.up;
+                else return from.left;
+            default:
+                return from;
+        }
+    }
+
+    static bool CheckStart(Vector2Int from, int counter){
+        if (Input[from.Y][from.X].ToString() == "S" && counter > 2){
+            return true;
+        }
+        return false;
+    }
+
+    static bool IsVectorInList(Vector2Int vec, List<Vector2Int> list){
+        Vector2Int? v = list.Find(v => v.X == vec.X && v.Y == vec.Y);
+
+        if (v == null){
+            return false;
+        }
+
+        return true;
+    }
+
+    static List<Vector2Int> GetLoopPositions(Vector2Int from, int counter, List<Vector2Int> visited){
+        if (from.X < 0 || from.X >= Input[0].Length || from.Y < 0 || from.Y >= Input.Count){
+            return new List<Vector2Int>();
+        }
+
+        visited.Add(new Vector2Int(from.X, from.Y));
+
+        if (Input[from.Y][from.X].ToString() == "S" && counter != 1){
+            return visited;
+        }
+
+        Vector2Int next = VectorToCheck(from, visited, counter);
+        if (next == from){
+            return new List<Vector2Int>();
+        }
+        
+        return GetLoopPositions(next, ++counter, visited);
+    }
+
+    static void Part1(){
+        FindLoop();
+        Console.WriteLine(loop.Count / 2);
+    }
+
+    static void Part2(){
+        int counter = 0;
+        bool isOpen = false;
+        for (int i = 0; i < Input.Count - 1; i++){
+            for (int j = 0; j < Input[i].Length; j++){
+                if (isOpen){
+                    if (IsVectorInList(new Vector2Int(j, i), loop)) {
+                        if ((Input[i][j] == 'S' || Input[i][j] == 'F' || Input[i][j] == '|' || Input[i][j] == '7') && 
+                            (Input[i + 1][j] == 'S' || Input[i + 1][j] == '|' || Input[i + 1][j] == 'J' || Input[i + 1][j] == 'L')){
+                            isOpen = false;
+                        }
+                    }
+                    else{
+                        counter++;
+                    }
+                }
+                else{
+                    if (IsVectorInList(new Vector2Int(j, i), loop)) {
+                        if ((Input[i][j] == 'S' || Input[i][j] == 'F' || Input[i][j] == '|' || Input[i][j] == '7') && 
+                            (Input[i + 1][j] == 'S' || Input[i + 1][j] == '|' || Input[i + 1][j] == 'J' || Input[i + 1][j] == 'L')){
+                            isOpen = true;
+                        }
+                    }
+                }
+            }
+            isOpen = false;
+        }
+
+        Console.WriteLine(counter);
+    }
+
+    //Part 1: 7066
+    //Part 2: 401 goal
+              
+    public static void Main(string[] args){
+        Input = ReadFile();
+        Part1();
+        Part2();
+    }
+}
+
+class Vector2Int{
+    public int X;
+    public int Y;
+
+    public Vector2Int(int x, int y){
+        X = x;
+        Y = y;
+    }
+
+    public void PrintVector(){
+        Console.WriteLine("(" + X.ToString() + ", " + Y.ToString() + ")");
+    }
+    public Vector2Int left{
+        get{return new Vector2Int(X - 1, Y);}
+    }
+    public Vector2Int right{
+        get{return new Vector2Int(X + 1, Y);}
+    }
+    public Vector2Int up{
+        get{return new Vector2Int(X, Y - 1);}
+    }
+    public Vector2Int down{
+        get{return new Vector2Int(X, Y + 1);}
+    }
+}
