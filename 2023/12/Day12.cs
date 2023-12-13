7746952e26cbef329e6c966bfcc74a5bd84bbdd3
Slashstep <slashstep@outlook.de>
2023-12-13 13:58:50 +0100

    Day 12, Day 13

--- 2023/12/Day12.cs
+++ 2023/12/Day12.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Diagnostics;
+using Microsoft.Extensions.Caching.Memory;
+
+/*
+Credits to https://github.com/LiquidFun
+*/
 
 class Day12{
 
@@ -30,9 +36,9 @@ class Day12{
         string[] leftRight = Input[index].Split(" ");
         string[] Right = leftRight[1].Split(",");
 
-        int[] nums = new int[Right.Length];
-        for (int i = 0; i < nums.Length; i++){
-            nums[i] = int.Parse(Right[i]);
+        Queue<int> nums = new Queue<int>();
+        for (int i = 0; i < nums.Count; i++){
+            nums.Enqueue(int.Parse(Right[i]));
         }
 
         HotSpringRow hsr = new HotSpringRow(leftRight[0], nums);
@@ -44,10 +50,10 @@ class Day12{
         string[] Right = leftRight[1].Split(",");
         string left = leftRight[0] + "?" + leftRight[0] + "?" + leftRight[0] + "?" + leftRight[0] + "?" + leftRight[0];
 
-        int[] nums = new int[Right.Length * 5];
-        for (int i = 0; i < Right.Length; i++){
-            for (int j = 0; j < 5; j++){
-                nums[Right.Length * j + i] = int.Parse(Right[i]);
+        Queue<int> nums = new Queue<int>();
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < Right.Length; j++){
+                nums.Enqueue(int.Parse(Right[j]));
             }
         }
 
@@ -60,25 +66,30 @@ class Day12{
         HotSpringRow hsr;
         for(int i = 0; i < Input.Count; i++){
             hsr = GetRow(i);
-            counter += hsr.CheckString("", 0);
+            //counter += hsr.CheckString("", 0);
         }
 
         Console.WriteLine(counter);
     }
 
     static void Part2(){
+        var watch = new Stopwatch();
+        watch.Start();
+
         long counter = 0;
         HotSpringRow hsr;
         for(int i = 0; i < Input.Count; i++){
             hsr = GetUnfoldedRow(i);
-            counter += hsr.CheckString("", 0);
+            counter += hsr.CheckStringCache(hsr.Source, hsr.Arrangement, 0);
         }
 
         Console.WriteLine(counter);
+        watch.Stop();
+        Console.WriteLine(watch.Elapsed);
     }
 
     //Part 1: 7286
-    //Part 2: 25470469710341 goal
+    //Part 2: 25470469710341
               
     public static void Main(string[] args){
         Input = ReadFile();
@@ -89,130 +100,175 @@ class Day12{
 
 class HotSpringRow{
     public string Source;
-    public int[] Arrangement;
+    public Queue<int> Arrangement;
     public int Possabilities;
-    Dictonary<string, int> cache;
+    Dictionary<string, long> cache;
+    //public IMemoryCache cache;
 
-    public HotSpringRow(string source, int[] arrangement){
+    public HotSpringRow(string source, Queue<int> arrangement){
         Source = source;
         Arrangement = arrangement;
         Possabilities = 0;
-        cache = new Dictonary<string, int>();
+        cache = new Dictionary<string, long>();
+        //cache = new MemoryCache(new MemoryCacheOptions());
     }
 
-    public long CheckString(string permutation, int index){
-        if (index == Source.Length){
-            if (IsArrangementValid(permutation))
-                return 1;
-            else
-                return 0;
+    private static string GetCacheKey((string, int, int) key)
+    {
+        return key.Item1 + key.Item2.ToString() + key.Item3.ToString();
+    }
+
+    public long CheckStringCache(string permutation, Queue<int> nums, int curHash){
+        (string, int, int) newTup = (permutation, nums.Count, curHash);
+        if (cache.TryGetValue(GetCacheKey(newTup), out long value)) return value;
+        //if (cache.ContainsKey(newTup)) return cache[newTup];
+
+        if (permutation.Length == 0){
+            if (nums.Count == 0 && curHash == 0) return 1;
+            else if (nums.Count == 1 && nums.Peek() == curHash) return 1;
+            else return 0;
         }
 
         long counter = 0;
-        if (Source[index] =='.' || Source[index] == '#'){
-            if (IsUnfinishedArrangementValid(permutation + Source[index]))
-                counter = CheckString(permutation + Source[index], ++index);
-        }
-        else{
-            if (IsUnfinishedArrangementValid(permutation + "."))
-                counter = CheckString(permutation + ".", index + 1);
-            if (IsUnfinishedArrangementValid(permutation + "#"))
-                counter = counter + CheckString(permutation + "#", index + 1);
+        if (permutation[0] == '?'){
+            counter += CheckStringCache("." + permutation.Substring(1), nums, curHash);
+            counter += CheckStringCache("#" + permutation.Substring(1), nums, curHash);
         }
 
+        if (permutation[0] == '#' && nums.Count > 0 && curHash < nums.Peek()){
+            counter += CheckStringCache(permutation.Substring(1), nums, curHash + 1);
+        }
+
+        if (permutation[0] == '.'){
+            if (nums.Count > 0 && curHash == nums.Peek()){
+                Queue<int> newQueue = new Queue<int>(nums);
+                newQueue.Dequeue();
+                counter += CheckStringCache(permutation.Substring(1), newQueue, 0);
+            }
+            
+            if (nums.Count == 0 || curHash == 0){
+                counter += CheckStringCache(permutation.Substring(1), nums, 0);
+            }
+        }
+
+        cache.Add(GetCacheKey(newTup), counter);
+        //cache.Set(GetCacheKey(newTup), counter, TimeSpan.FromMinutes(10));
         return counter;
     }
 
-    public int CheckUnfinishedPermutations(){
-        List<string> permutations = new List<string>();
-        permutations.Add("");
+    // public long CheckString(string permutation, int index){
+    //     if (index == Source.Length){
+    //         if (IsArrangementValid(permutation))
+    //             return 1;
+    //         else
+    //             return 0;
+    //     }
 
-        for (int i = 0; i < Source.Length; i++){
-            if (Source[i] == '.' || Source[i] == '#'){
-                for (int j = 0; j < permutations.Count; j++){
-                    permutations[j] += Source[i];
-                }
-            }
-            // ?
-            else{
-                List<string> newPerm = new List<string>();
-                for (int j = 0; j < permutations.Count; j++){
-                    if (IsUnfinishedArrangementValid(permutations[j] + ".")) newPerm.Add(permutations[j] + ".");
-                    if (IsUnfinishedArrangementValid(permutations[j] + "#")) newPerm.Add(permutations[j] + "#");
-                }
-                newPerm.RemoveAll(p => p.Length == i);
-                permutations = newPerm;
-            }
-        }
+    //     long counter = 0;
+    //     if (Source[index] =='.' || Source[index] == '#'){
+    //         if (IsUnfinishedArrangementValid(permutation + Source[index]))
+    //             counter = CheckString(permutation + Source[index], ++index);
+    //     }
+    //     else{
+    //         if (IsUnfinishedArrangementValid(permutation + "."))
+    //             counter = CheckString(permutation + ".", index + 1);
+    //         if (IsUnfinishedArrangementValid(permutation + "#"))
+    //             counter = counter + CheckString(permutation + "#", index + 1);
+    //     }
 
-        int counter = 0;
-        foreach (string s in permutations){
-            if (IsArrangementValid(s)) counter++;
-        }
+    //     return counter;
+    // }
 
-        return counter;
-    }
+    // public int CheckUnfinishedPermutations(){
+    //     List<string> permutations = new List<string>();
+    //     permutations.Add("");
 
-    bool IsUnfinishedArrangementValid(string pos){
-        bool isWaitingForDot = false;
-        int arrangementIndex = -1;
-        int arrangementAmount = 0;
-        for (int i = 0; i < pos.Length; i++){
-            if (pos[i] == '#'){
-                //start new sequence
-                if (arrangementAmount == 0 && !isWaitingForDot){
-                    arrangementIndex++;
+    //     for (int i = 0; i < Source.Length; i++){
+    //         if (Source[i] == '.' || Source[i] == '#'){
+    //             for (int j = 0; j < permutations.Count; j++){
+    //                 permutations[j] += Source[i];
+    //             }
+    //         }
+    //         // ?
+    //         else{
+    //             List<string> newPerm = new List<string>();
+    //             for (int j = 0; j < permutations.Count; j++){
+    //                 if (IsUnfinishedArrangementValid(permutations[j] + ".")) newPerm.Add(permutations[j] + ".");
+    //                 if (IsUnfinishedArrangementValid(permutations[j] + "#")) newPerm.Add(permutations[j] + "#");
+    //             }
+    //             newPerm.RemoveAll(p => p.Length == i);
+    //             permutations = newPerm;
+    //         }
+    //     }
+
+    //     int counter = 0;
+    //     foreach (string s in permutations){
+    //         if (IsArrangementValid(s)) counter++;
+    //     }
+
+    //     return counter;
+    // }
+
+    // bool IsUnfinishedArrangementValid(string pos){
+    //     bool isWaitingForDot = false;
+    //     int arrangementIndex = -1;
+    //     int arrangementAmount = 0;
+    //     for (int i = 0; i < pos.Length; i++){
+    //         if (pos[i] == '#'){
+    //             //start new sequence
+    //             if (arrangementAmount == 0 && !isWaitingForDot){
+    //                 arrangementIndex++;
                     
-                    if (arrangementIndex == Arrangement.Length) return false;
-                    arrangementAmount = Arrangement[arrangementIndex] - 1;
-                    isWaitingForDot = true;
-                }
-                //# after sequence End
-                else if (arrangementAmount == 0 && isWaitingForDot) return false;
-                else {
-                    arrangementAmount--;
-                }
-            }
-            else{
-                if (arrangementAmount > 0) return false;
+    //                 if (arrangementIndex == Arrangement.Count) return false;
+    //                 arrangementAmount = Arrangement[arrangementIndex] - 1;
+    //                 isWaitingForDot = true;
+    //             }
+    //             //# after sequence End
+    //             else if (arrangementAmount == 0 && isWaitingForDot) return false;
+    //             else {
+    //                 arrangementAmount--;
+    //             }
+    //         }
+    //         else{
+    //             if (arrangementAmount > 0) return false;
 
-                isWaitingForDot = false;
-            }
-        }
+    //             isWaitingForDot = false;
+    //         }
+    //     }
 
-        return true;
-    }
+    //     return true;
+    // }
 
-    bool IsArrangementValid(string pos){
-        bool isWaitingForDot = false;
-        int arrangementIndex = -1;
-        int arrangementAmount = 0;
-        for (int i = 0; i < pos.Length; i++){
-            if (pos[i] == '#'){
-                //start new sequence
-                if (arrangementAmount == 0 && !isWaitingForDot){
-                    arrangementIndex++;
+    // bool IsArrangementValid(string pos){
+    //     bool isWaitingForDot = false;
+    //     int arrangementIndex = -1;
+    //     int arrangementAmount = 0;
+    //     for (int i = 0; i < pos.Length; i++){
+    //         if (pos[i] == '#'){
+    //             //start new sequence
+    //             if (arrangementAmount == 0 && !isWaitingForDot){
+    //                 arrangementIndex++;
                     
-                    if (arrangementIndex == Arrangement.Length) return false;
-                    arrangementAmount = Arrangement[arrangementIndex] - 1;
-                    isWaitingForDot = true;
-                }
-                //# after sequence End
-                else if (arrangementAmount == 0 && isWaitingForDot) return false;
-                else {
-                    arrangementAmount--;
-                }
-            }
-            else{
-                if (arrangementAmount > 0) return false;
+    //                 if (arrangementIndex == Arrangement.Length) return false;
+    //                 arrangementAmount = Arrangement[arrangementIndex] - 1;
+    //                 isWaitingForDot = true;
+    //             }
+    //             //# after sequence End
+    //             else if (arrangementAmount == 0 && isWaitingForDot) return false;
+    //             else {
+    //                 arrangementAmount--;
+    //             }
+    //         }
+    //         else{
+    //             if (arrangementAmount > 0) return false;
 
-                isWaitingForDot = false;
-            }
-        }
+    //             isWaitingForDot = false;
+    //         }
+    //     }
 
-        if (arrangementIndex != Arrangement.Length - 1) return false;
-        if (arrangementAmount > 0) return false;
+    //     if (arrangementIndex != Arrangement.Length - 1) return false;
+    //     if (arrangementAmount > 0) return false;
 
-        return true;
-    }
+    //     return true;
+    // }
 }
---
+++ 2023/13/Day13.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Diagnostics;
+
+class Day13{
+
+    static public List<string> Input = new List<string>();
+
+    static List<string> ReadFile(){
+        List<string> lines = new List<string>();
+
+        try{
+            if (File.Exists("input.txt"))
+                lines.AddRange(File.ReadAllLines("input.txt"));
+            else
+                Console.WriteLine("The file does not exist.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+
+        return lines;
+    }
+
+    static List<Pattern> GetPatterns(){
+        List<Pattern> patterns = new List<Pattern>();
+        List<string> lines = new List<string>();
+
+        for (int i = 0; i < Input.Count; i++){
+            if (Input[i] == ""){
+                patterns.Add(new Pattern(new List<string>(lines)));
+                lines.Clear();
+            }
+            else
+            {
+                lines.Add(Input[i]);
+            }
+        }
+        patterns.Add(new Pattern(new List<string>(lines)));
+        return patterns;
+    }
+
+    static List<SmudgePattern> GetSmudgePatterns()
+    {
+        List<SmudgePattern> patterns = new List<SmudgePattern>();
+        List<string> lines = new List<string>();
+
+        for (int i = 0; i < Input.Count; i++)
+        {
+            if (Input[i] == "")
+            {
+                patterns.Add(new SmudgePattern(new List<string>(lines)));
+                lines.Clear();
+            }
+            else
+            {
+                lines.Add(Input[i]);
+            }
+        }
+        patterns.Add(new SmudgePattern(new List<string>(lines)));
+        return patterns;
+    }
+
+    static void Part1(){
+        List<Pattern> patterns = GetPatterns();
+
+        int counter = 0;
+        foreach (Pattern p in patterns)
+        {
+            counter += p.TotalValue;
+        }
+
+        Console.WriteLine(counter);
+    }
+
+    static void Part2(){
+        List<SmudgePattern> patterns = GetSmudgePatterns();
+
+        int counter = 0;
+        foreach (SmudgePattern p in patterns)
+        {
+            counter += p.TotalValue;
+            Console.WriteLine(p.TotalValue);
+        }
+
+        Console.WriteLine(counter);
+    }
+
+    //Part 1: 42974
+    //Part 2: 27587
+
+    public static void Main(string[] args){
+        Input = ReadFile();
+        Part1();
+        Part2();
+    }
+}
+
+class Pattern{
+    public List<string> Lines = new List<string>();
+    public int VerticalValue;
+    public int HorizontalValue;
+    public int TotalValue;
+
+    public Pattern(List<string> lines){
+        Lines = lines;
+        VerticalValue = CheckVertical();
+        HorizontalValue = CheckHorizontal();
+        if (VerticalValue > HorizontalValue) TotalValue = VerticalValue;
+        else TotalValue = 100 * HorizontalValue;
+    }
+
+    public int CheckVertical()
+    {
+        List<string> LinesTurned = new List<string>();
+        List<string> verticalLines = new List<string>();
+        List<(int, int)> symmetries = new List<(int, int)>();
+
+        for (int i = 0; i < Lines[0].Length; i++)
+        {
+            string s = "";
+
+            for (int y = 0; y < Lines.Count; y++)
+                s += Lines[y][i];
+
+            LinesTurned.Add(s);
+        }
+
+        for (int i = 0; i < LinesTurned.Count; i++)
+        { 
+            if (verticalLines.Count > 0 && verticalLines.Last() == LinesTurned[i])
+            {
+                for (int j = 0; j < LinesTurned.Count; j++)
+                {
+                    if (verticalLines.Count - 1 - j < 0 || i + j >= LinesTurned.Count)
+                    {
+                        symmetries.Add((i, j));
+                        break;
+                    }
+
+                    if (verticalLines[verticalLines.Count - 1 - j] != LinesTurned[i + j])
+                        break;
+                }
+            }
+
+            verticalLines.Add(LinesTurned[i]);
+        }
+
+        symmetries = symmetries.OrderByDescending(t => t.Item2).ToList();
+
+        if (symmetries.Count == 0) return 0;
+        else return symmetries[0].Item1;
+    }
+
+    public int CheckHorizontal()
+    {
+        List<string> horizontalLines = new List<string>();
+        List<(int, int)> symmetries = new List<(int, int)>();
+
+        for (int i = 0; i < Lines.Count; i++)
+        {
+            if (horizontalLines.Count > 0 && horizontalLines.Last() == Lines[i])
+            {
+                for (int j = 0; j < Lines.Count; j++)
+                {
+                    if (horizontalLines.Count - 1 - j < 0 || i + j >= Lines.Count)
+                    {
+                        symmetries.Add((i, j));
+                        break;
+                    }
+
+                    if (horizontalLines[horizontalLines.Count - 1 - j] != Lines[i + j])
+                        break;
+                }
+            }
+
+            horizontalLines.Add(Lines[i]);
+        }
+
+        symmetries = symmetries.OrderByDescending(t => t.Item2).ToList();
+
+        if (symmetries.Count == 0) return 0;
+        else return symmetries[0].Item1;
+    }
+}
+
+class SmudgePattern{
+    public List<string> Lines = new List<string>();
+    public int VerticalValue;
+    public int HorizontalValue;
+    public int TotalValue;
+
+    public SmudgePattern(List<string> lines){
+        Lines = lines;
+        VerticalValue = CheckVertical();
+        HorizontalValue = CheckHorizontal();
+        if (VerticalValue > HorizontalValue) TotalValue = VerticalValue;
+        else TotalValue = 100 * HorizontalValue;
+    }
+
+    public int CheckVertical()
+    {
+        List<string> LinesTurned = new List<string>();
+        List<string> verticalLines = new List<string>();
+        List<(int, int)> symmetries = new List<(int, int)>();
+
+        for (int i = 0; i < Lines[0].Length; i++)
+        {
+            string s = "";
+
+            for (int y = 0; y < Lines.Count; y++)
+                s += Lines[y][i];
+
+            LinesTurned.Add(s);
+        }
+
+        for (int i = 0; i < LinesTurned.Count; i++)
+        {
+            int smudgeCounter = 0;
+            for (int j = 0; j < LinesTurned.Count; j++)
+            {
+                if (verticalLines.Count - 1 - j < 0 || i + j >= LinesTurned.Count)
+                {
+                    if (smudgeCounter <= 1)
+                    {
+                        symmetries.Add((i, j));
+                        break;
+                    }
+                    else break;
+                }
+
+                if (verticalLines[verticalLines.Count - 1 - j] != LinesTurned[i + j])
+                {
+                    for (int k = 0; k < LinesTurned[i + j].Length; k++)
+                    {
+                        if (verticalLines[verticalLines.Count - 1 - j][k] != LinesTurned[i + j][k])
+                            smudgeCounter++;
+                    }
+
+                    if (smudgeCounter > 1) break;
+                }
+            }
+            verticalLines.Add(LinesTurned[i]);
+        }
+
+        symmetries = symmetries.OrderByDescending(t => t.Item2).ToList();
+
+        if (symmetries.Count == 0) return 0;
+        else
+        {
+            foreach ((int, int) i in symmetries)
+            {
+                Console.WriteLine(i.Item1.ToString() + ", " + i.Item2.ToString() + "vertical");
+            }
+            Console.WriteLine("");
+
+            Pattern newPat = new Pattern(Lines);
+            if (symmetries[0].Item1 == newPat.TotalValue)
+            {
+                if (symmetries.Count > 1) return symmetries[1].Item1;
+                else return 0;
+            }
+            else return symmetries[0].Item1;
+        } 
+    }
+
+    public int CheckHorizontal()
+    {
+        List<string> horizontalLines = new List<string>();
+        List<(int, int)> symmetries = new List<(int, int)>();
+
+        for (int i = 0; i < Lines.Count; i++)
+        {
+            int smudgeCounter = 0;
+            for (int j = 0; j < Lines.Count; j++)
+            {
+                if (horizontalLines.Count - 1 - j < 0 || i + j >= Lines.Count)
+                {
+                    if (smudgeCounter <= 1)
+                    {
+                        symmetries.Add((i, j));
+                        break;
+                    }
+                    else break;
+                }
+
+                if (horizontalLines[horizontalLines.Count - 1 - j] != Lines[i + j])
+                {
+                    for (int k = 0; k < Lines[i + j].Length; k++)
+                    {
+                        if (horizontalLines[horizontalLines.Count - 1 - j][k] != Lines[i + j][k])
+                            smudgeCounter++;
+                    }
+
+                    if (smudgeCounter > 1) break;
+                }
+            }
+            horizontalLines.Add(Lines[i]);
+        }
+
+        symmetries = symmetries.OrderByDescending(t => t.Item2).ToList();
+
+        if (symmetries.Count == 0) return 0;
+        else
+        {
+            foreach ((int, int) i in symmetries)
+            {
+                Console.WriteLine(i.Item1.ToString() + ", " + i.Item2.ToString() + "horizontal");
+            }
+            Console.WriteLine("");
+
+            Pattern newPat = new Pattern(Lines);
+            if (symmetries[0].Item1 * 100 == newPat.TotalValue)
+            {
+                if (symmetries.Count > 1) return symmetries[1].Item1;
+                else return 0;
+            }
+            else return symmetries[0].Item1;
+        }
+    }
+}
